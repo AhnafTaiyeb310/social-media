13da40f133a3666b703dcb03169684cee4a23319
AhnafTaiyeb310 <ahnaftaiyeb1@gmail.com>
2026-04-06 10:54:11 +0600

    tags added in admin

--- backend/apps/tags/admin.py
+++ backend/apps/tags/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Tag, TaggedItem
 
-# Register your models here.
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['tag']
+    search_fields = ['tag']
+
+@admin.register(TaggedItem)
+class TaggedItemAdmin(admin.ModelAdmin):
+    list_display = ['tag', 'content_type', 'object_id', 'content_object']
+    list_filter = ['content_type']
+    search_fields = ['tag__tag']
