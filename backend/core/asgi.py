339de69e907b55dadd44f224f57dfba52897b17d
AhnafTaiyeb310 <ahnaftaiyeb1@gmail.com>
2026-05-09 22:10:12 +0600

    chat fix

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,14 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from apps.chat.middleware import JWTAuthMiddlewareStack
-import apps.chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing consumers and routing.
 django_asgi_app = get_asgi_application()
+
+# Import routing and middleware AFTER get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.chat.middleware import JWTAuthMiddlewareStack
+import apps.chat.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
